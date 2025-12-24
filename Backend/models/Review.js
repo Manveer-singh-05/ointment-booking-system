f230ef6613f2fc58e417dfc10ce3463171b9ac07
Manver Singh <manveersingh0501l@gmail.com>
2025-12-24 11:03:04 +0530

    do changes

--- Backend/models/Review.js
+++ Backend/models/Review.js
@@ -1,13 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const ReviewSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//   professionalId: { type: mongoose.Schema.Types.ObjectId, ref: "Professional", required: true },
-//   rating: { type: Number, required: true, min: 1, max: 5 },
-//   reviewText: { type: String, default: "" },
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Review", ReviewSchema);
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
