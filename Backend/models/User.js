2b4095976e423d5d3b5ce52b465b01d0c32961d9
Manver Singh <manveersingh0501l@gmail.com>
2025-12-26 22:15:54 +0530

    do minimal changes

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,28 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const UserSchema = new mongoose.Schema({
-//   name: { type: String, required: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-
-//   phone: { type: String, default: "" },
-//   bio: { type: String, default: "" },
-//   photo: { type: String, default: "" },
-
-//   role: {
-//     type: String,
-//     enum: ["user", "admin"],
-//     default: "user"
-//   },
-
-//   lastLogin: { type: Date },
-
-//   // ⭐ ADD BELOW
-//   resetOTP: { type: String },
-//   resetOTPExpiry: { type: Date }
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("User", UserSchema);
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
